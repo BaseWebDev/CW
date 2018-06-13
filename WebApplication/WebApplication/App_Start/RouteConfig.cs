5589c14f8b9c115f26dd07ab4aad975d11e717bb
SermusAnton <antonsermus@mail.ru>
2018-06-13 12:21:01 +0300

    Fix RouteConfig

--- WebApplication/WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/WebApplication/App_Start/RouteConfig.cs
@@ -7,14 +7,16 @@ using System.Web.Routing;
 
 namespace WebApplication {
     public class RouteConfig {
+        const string prdContr = @"Products";
+        const string prdList = @"List";
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(null,
                 "",
                 new {
-                    controller = "Product",
-                    action = "List",
+                    controller = prdContr,
+                    action = prdList,
                     category = (string)null,
                     page = 1
                 }
@@ -23,18 +25,18 @@ namespace WebApplication {
             routes.MapRoute(
                 name: null,
                 url: "Page{page}",
-                defaults: new { controller = "Product", action = "List", category = (string)null },
+                defaults: new { controller = prdContr, action = prdList, category = (string)null },
                 constraints: new { page = @"\d+" }
             );
 
             routes.MapRoute(null,
                 "{category}",
-                new { controller = "Product", action = "List", page = 1 }
+                new { controller = prdContr, action = prdList, page = 1 }
             );
 
             routes.MapRoute(null,
                 "{category}/Page{page}",
-                new { controller = "Product", action = "List" },
+                new { controller = prdContr, action = prdList },
                 new { page = @"\d+" }
             );
 
